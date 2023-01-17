4eb56286c5786de1f7c254ebec3a8e1f254d55d5
Paulo Mariano <paulomariano2501@gmail.com>
2023-01-17 10:57:25 -0300

    adding tailwind on frontend project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="text-xl bg-indigo-800 ">
+     faaaaaaaaaaaaaaf
     </div>
   )
 }
